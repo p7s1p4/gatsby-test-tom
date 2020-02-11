9e098e0b6dbcfdccc79a1d367a37fd591097fb94
Thomas Wunderlich <tms.wunderlich@gmail.com>
2020-02-11 11:57:55 +0100

    custom page / sass

---
+++ src/pages/custom.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Custom = () => (
+  <div>
+    <h1>Custom</h1>
+    <p>At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+  </div>
+)
+
+export default Custom
